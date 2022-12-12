599e711801cf131efb94eeb03f693283a033ea15
kostisPodaras <podaraskostis@gmail.com>
2022-12-12 09:53:10 +0200

    Add modal close functionality on click outside

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,16 +1,26 @@
 // @ts-nocheck
+import { useEffect } from 'react';
 import ReactDom from 'react-dom';
 import styles from './Modal.module.css';
 
 export const Modal = ({ isOpen, children, onClose }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return ReactDom.createPortal(
     <>
-      <div className={styles.overlay} />
-      <div className={styles.modal}>
-        <button onClick={onClose}>Close Modal</button>
-        {children}
+      <div onClick={onClose} className={styles.overlay}>
+        <div onClick={(e) => e.stopPropagation()} className={styles.modal}>
+          <button onClick={onClose}>Close Modal</button>
+          {children}
+        </div>
       </div>
     </>,
     document.getElementById('portal'),
